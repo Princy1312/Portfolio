b21fdabfc5d9ddfc729bf4d613cbcdeae0709355
princy1312 <notahiana.princy@gmail.com>
2025-08-12 09:14:43 +0100

    premier commit

---
+++ scripts.js
@@ -0,0 +1,11 @@
+const navLinks = document.querySelectorAll('.nav-links li');
+const slider = document.querySelector('.slider');
+navLinks.forEach((link,index) =>  {
+    link.addEventListener('mouseover',() => {
+        const linkRect = link.getBoundingClientRect();
+        const parentRect = 
+        link.parentElement.getBoundingClientRect ();
+        slider.style.width =`${linkRect.width}px`;
+        slider.style.transform = `translateX(${link.offsetLeft}px)`;
+    });
+});
